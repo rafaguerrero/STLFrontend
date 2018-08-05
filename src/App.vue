9e505ecc9113e51b1e784b57dcb9e89ce630d117
rafaelguerreroclemente <rafa.guerrero@gmail.com>
2018-08-05 12:10:16 +0200

    Vue structure

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="stl-greeting">
+        <p class="stl-greeting--title">Test : {{ greeting }} World!</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'App',
+        data: function () {
+            return { greeting: 'Hello' }
+        }
+    };
+</script>
+
+<style lang="scss" scoped>
+    .stl-greeting {
+        font-size: 2em;
+        text-align: center;
+
+        &--title {
+            color : blue;
+        }
+    }
+</style>
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,39 @@
+'use strict'
+const path = require('path')
+
 module.exports = {
+    entry: {
+        app: './src/main.js'
+    },
+    resolve: {
+        extensions: ['.js', '.vue', '.json'],
+        alias: {
+          'vue$': 'vue/dist/vue.esm.js',
+          '@': path.join(__dirname, '..', 'src')
+        }
+    },
     module: {
         rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: {
                     loader: "babel-loader"
                 }
+            },
+            {
+              test: /\.scss$/,
+              use: [
+                'vue-style-loader',
+                'css-loader',
+                {
+                  loader: 'sass-loader',
+                },
+              ],
             }
         ]
     }
